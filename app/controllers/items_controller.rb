96a5ce2d3ae61f83fff1b0d7cbf30201082695c2
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2021-02-21 09:31:16 +0900

    商品出品機能のMVCを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -19,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, ,:item_info,:category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :item_info,:category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,15 @@ class Item < ApplicationRecord
   belongs_to :schedule 
   has_one_attached :image
 
-  validates :title, :text, presence: true
-
-  validates :category_id,     numericality: { other_than: 1 }
-  validates :status_id,       numericality: { other_than: 1 }
-  validates :shipping_fee_id, numericality: { other_than: 1 }
-  validates :prefecture_id,   numericality: { other_than: 1 }
-  validates :schedule_id,     numericality: { other_than: 1 }
+  with_options presence: true do
+   validates :image
+   validates :title, :text
+   validates :category_id,     numericality: { other_than: 1 }
+   validates :status_id,       numericality: { other_than: 1 }
+   validates :shipping_fee_id, numericality: { other_than: 1 }
+   validates :prefecture_id,   numericality: { other_than: 1 }
+   validates :schedule_id,     numericality: { other_than: 1 }
+   validates :price, format: {with: /\A[0-9]+\z/, message: '半角数字を使用してください'}
+   validates_inclusion_of :price, in:300..9999999
+  end 
 end
