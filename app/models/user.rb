7f2341796c50cd3edca980aff27d84c9503b651a
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2021-02-22 22:00:40 +0900

    コードのインデントなどを微調整、不要な記述をコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- with_options presence: true do
+  with_options presence: true do
     validates :nickname 
     validates :email, format: {with: /@.+/, message: 'メールアドレスには@を入れてください'}
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字を使用してください' }
