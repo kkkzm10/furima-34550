2e26031ea6b0e9bec7ad4611a95b812e045b0a2a
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2021-02-28 07:46:00 +0900

    テストコードの微修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  describe '購入情報の保存' do
+  
     before do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
       sleep 0.1 
     end
-
+  describe '購入情報の保存' do
+   context '商品が購入できるとき' do
     it 'すべての値が正しく入力されていれば保存できること' do
       expect(@order_address).to be_valid
     end
+   end 
+   context '商品が購入できないとき' do
     it 'postal_codeが空だと保存できないこと' do
       @order_address.postal_code = ''
       @order_address.valid?
@@ -47,5 +50,6 @@ RSpec.describe OrderAddress, type: :model do
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Token can't be blank")
     end
+   end 
   end
 end
