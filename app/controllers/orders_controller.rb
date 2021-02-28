29dfc2229769e29adf441d9721e61cbb2eec65dd
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2021-02-28 16:09:43 +0900

    コントローラーに記述を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,19 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index, :create]
 
-  def index
+  def set_item
     @item = Item.find(params[:item_id])
-     if current_user.id == @item.user_id
+  end
+  
+  def index
+     if current_user.id == @item.user_id || @item.order.present?
        redirect_to root_path
      end 
     @order_address = OrderAddress.new
   end
 
  def create
-  @item = Item.find(params[:item_id])
   @order_address = OrderAddress.new(order_params)
   if @order_address.valid?
     pay_item
