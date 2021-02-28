27393dbef118375444dc15d2d32751cace7b0257
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2021-02-28 14:45:12 +0900

    order_controllerを微修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
+     if current_user.id == @item.user_id
+       redirect_to root_path
+     end 
     @order_address = OrderAddress.new
   end
 
